bd91006e359a271fc833d0d6bceb8ca85f20400b
Hajime Suzuki <yline5050hs@gmail.com>
2016-05-12 01:39:05 +0900

    update lmm.h

--- lmm.h
+++ lmm.h
@@ -319,8 +319,8 @@ int main() {
 	: 0), (v).a[(i)])
 
 /** bound-unchecked accessor */
-#define lmm_kv_at(v, i) ( (v).a[(i)] )
-#define lmm_kv_ptr(v)  ( (v).a )
+#define lmm_kv_at(v, i)			( (v).a[(i)] )
+#define lmm_kv_ptr(v)			( (v).a )
 
 /** heap queue : elements in v must be orderd in heap */
 #define lmm_kv_hq_init(lmm, v)		{ lmm_kv_init(lmm, v); (v).n = 1; }
